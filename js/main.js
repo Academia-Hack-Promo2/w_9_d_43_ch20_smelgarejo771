0019c4f43b5f280b8be6d1f587101d48ebed2d2e
samuel melgarejo <samuel_021093@hotmail.com>
2015-04-15 22:36:23 -0430

    primer comid(mitad)

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 500) {
-        $("header").addClass("show");
-    } else {
-        $("header").removeClass("show");
-    }
-});
